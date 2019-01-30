f66e2d1240979ae95eea969c73bd2c341218cf1d
Alexis J Carr <34029816+alexisjcarr@users.noreply.github.com>
2019-01-29 20:16:17 -0600

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-//import logo from './logo.svg';
-//import './App.css';
+//import { Cube } from "react-preloaders";
 import { Pomodoro } from "./Pomodoro/Pomodoro.js";
 
 class App extends Component {
   render() {
     return (
-      <div className="root">
+      <div>
         <Pomodoro />
       </div>
     );
